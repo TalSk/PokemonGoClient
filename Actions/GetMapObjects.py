9880e5bb7111c0e25bc6d04591a6a42d57bf0b3b
Tal Skverer <taltaltal1994@gmail.com>
2016-07-18 01:06:01 +0300

    Refactor of the proto files

--- Actions/GetMapObjects.py
+++ Actions/GetMapObjects.py
@@ -1,8 +1,10 @@
-import request_pb2, response_pb2
 from Util import Utils, NetUtil, Constants
-		
+import ResponseEnvelop_pb2
+from Requests import GetMapObjectsRequest_pb2
+from Responses import GetMapObjectsResponse_pb2
 
-class GetMapObjectsRequest(object):
+
+class GetMapObjects(object):
 	
 	def __init__(self, raw_request, url, logger=None):
 		self.request = raw_request
@@ -11,9 +13,9 @@ class GetMapObjectsRequest(object):
 		
 
 	def _handle_response(self, response):
-		response_envelop = response_pb2.ResponseEnvelop()
+		response_envelop = ResponseEnvelop_pb2.ResponseEnvelop()
 		response_envelop.ParseFromString(response.content)
-		get_map_objects_response = response_pb2.PossibleResponse1()
+		get_map_objects_response = GetMapObjectsResponse_pb2.GetMapObjectsResponse()
 		get_map_objects_response.ParseFromString(response_envelop.responses[0])
 		assert get_map_objects_response
 
@@ -23,12 +25,12 @@ class GetMapObjectsRequest(object):
 
 
 	def get(self, latitude, longitude, cell_ids):
-		get_map_objects_request = request_pb2.RequestEnvelop.GetMapObjectsRequest()
+		get_map_objects_request = GetMapObjectsRequest_pb2.GetMapObjectsRequest()
 		for cell_id in cell_ids:
 		 	get_map_objects_request.cell_id.append(cell_id.id())
 		 	get_map_objects_request.since_time_ms.append(0)
-		get_map_objects_request.player_lat = latitude
-		get_map_objects_request.player_lng = longitude
+		get_map_objects_request.latitude = latitude
+		get_map_objects_request.longitude = longitude
 
 		self.request.requests[0].message = get_map_objects_request.SerializeToString()
 		data = self.request.SerializeToString()
--- Auth/GoogleLogin.py
+++ Auth/GoogleLogin.py
@@ -1,5 +1,5 @@
 import time
-import request_pb2, response_pb2
+import RequestEnvelop_pb2, ResponseEnvelop_pb2, EnvelopData_pb2
 from Util import NetUtil, Constants, Utils
 from JWTTokenReceiver import JWTTokenReceiver
 
@@ -30,37 +30,37 @@ class GoogleLogin(object):
 		with open(self.CACHE_FILE, "rb") as f:
 			f.readline()
 			url = f.readline()[:-2] # To remove \r\n
-			token_data = response_pb2.ResponseEnvelop.TokenData()
+			auth_ticket = EnvelopData_pb2.AuthTicket()
 			token = f.readline()[:-2]
-			token_data.token = token
+			auth_ticket.token = token
 			timestamp = f.readline()[:-2]
-			token_data.timestamp = long(timestamp)
+			auth_ticket.expire_timestamp_ms = long(timestamp)
 			sig = f.readline()[:-2]
-			token_data.sig = sig
-			return url, token_data
+			auth_ticket.sig = sig
+			return url, auth_ticket
 
 
 	def _cache_login(self):
 		with open(self.CACHE_FILE, "wb") as f:
 			f.write(str(time.time()) + "\r\n")
 			f.write(self.url + "\r\n")
-			f.write(self.session_token.token + "\r\n")
-			f.write(str(self.session_token.timestamp) + "\r\n")
-			f.write(self.session_token.sig + "\r\n")
+			f.write(self.auth_ticket.token + "\r\n")
+			f.write(str(self.auth_ticket.expire_timestamp_ms) + "\r\n")
+			f.write(self.auth_ticket.sig + "\r\n")
 
 
-	def _get_session_token(self, jwt_token):
+	def _get_auth_ticket(self, jwt_token):
 		# Creating a raw request
-		req_env = request_pb2.RequestEnvelop()
-		req_env.unknown1 = 2
+		req_env = RequestEnvelop_pb2.RequestEnvelop()
+		req_env.status_code = 2
 		req_env.rpc_id = Utils.randomize_rpc_id()
 		# It doesn't matter what kind of response you request here, you won't get any response. You must send at least one, though.
 		request = req_env.requests.add()
-		request.type = Constants.GET_MAP_OBJECTS_REQUEST
+		request.request_type = Constants.GET_MAP_OBJECTS_REQUEST
 		# Adding the authentication details
-		req_env.auth.provider = "google"
-		req_env.auth.token.contents = jwt_token
-		req_env.time_delta = 1036 # TODO
+		req_env.auth_info.provider = "google"
+		req_env.auth_info.token.contents = jwt_token
+		req_env.unknown12 = 1036 # TODO
 
 		raw_data = req_env.SerializeToString()
 		if self.logger:
@@ -69,30 +69,30 @@ class GoogleLogin(object):
 
 
 	def _handle_response(self, response):
-		res_pb = response_pb2.ResponseEnvelop()
+		res_pb = ResponseEnvelop_pb2.ResponseEnvelop()
 		res_pb.ParseFromString(response.content)
-		assert res_pb.endpoint
-		assert res_pb.token
-		# The endpoint we recieve doesn't have a scheme prefix, or the rpc suffix.
-		return "https://" + res_pb.endpoint + "/rpc", res_pb.token
+		assert res_pb.api_url
+		assert res_pb.auth_ticket
+		# The url we recieve doesn't have a scheme prefix, or the rpc suffix.
+		return "https://" + res_pb.api_url + "/rpc", res_pb.auth_ticket
 
 
 	def _authenticate(self, jwt_token):
-		response = self._get_session_token(jwt_token)
+		response = self._get_auth_ticket(jwt_token)
 		return self._handle_response(response)
 
 
 	def login(self):
 		if self._has_cache_login():
-			self.url, self.session_token = self._get_cache_login()
+			self.url, self.auth_ticket = self._get_cache_login()
 		else:
 			jwt_token = JWTTokenReceiver(self.email, self.oauth_token, self.logger).get_token()
-			self.url, self.session_token = self._authenticate(jwt_token)
+			self.url, self.auth_ticket = self._authenticate(jwt_token)
 			self._cache_login()
 
 		assert self.url
-		assert self.session_token
+		assert self.auth_ticket
 		if self.logger:
-			self.logger.debug("Received endpoint url and token:\r\n%s\r\n%s" % (self.url, self.session_token))
+			self.logger.debug("Received endpoint url and token:\r\n%s\r\n%s" % (self.url, self.auth_ticket))
 		
-		return self.url, self.session_token
+		return self.url, self.auth_ticket
